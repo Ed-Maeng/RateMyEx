6dd8ed0b4f66fbeb721d3d4f876d94e8df3dcf8b
kmyohan0 <kmyohan0@gmail.com>
2023-05-11 19:56:51 -0700

    Fixed issue where search by Id in mongoose does not work

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,8 +3,8 @@ import User from "../models/User.js";
 export const getUser = async(req, res) => {
     try {
         const { userId } = req.params;
-        const user = await User.find({ userId });
-        console.log(user);
+        const _id = userId;
+        const user = await User.findById({ _id });
         res.status(200).json(user);
         } catch (err) {
         res.status(404).json({ message: err.message });

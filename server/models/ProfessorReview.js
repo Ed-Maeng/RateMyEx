d481bfe2c59ab4c1549496d81f84f45e2c136ece
kmyohan0 <kmyohan0@gmail.com>
2023-05-18 19:54:45 -0700

    Add Professor feature

---
+++ server/models/ProfessorReview.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const ProfessorReviewSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true
+    },
+    professorId: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true,
+      max: 50,
+    },
+    className: {
+      type: String,
+      required: true,
+      max: 50,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+    imageNames: {
+      type: Array,
+      default: [],
+    },
+    imageUrls: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const ProfessorReview = mongoose.model("ProfessorReview", ProfessorReviewSchema);
+export default ProfessorReview;
